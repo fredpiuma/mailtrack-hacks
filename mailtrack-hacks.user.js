13416d5f3d56f13a9e6e71183293bdcf3431a60f
fredpiuma <fredpiuma@gmail.com>
2016-10-12 03:53:53 +0000

    melhorias

--- mailtrack-hacks.user.js
+++ mailtrack-hacks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Gmail Remover Assinatura Mailtrack
+// @name         Gmail Hacks for Mailtrack and Asana
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  
 // @author       You
 // @match        https://mail.google.com/mail/u/*
@@ -36,6 +36,19 @@
 			if( deletar[i].length > 0 ) {
 				for(var j = 0; j < deletar[i].length; j++) {
 					deletar[i][j].remove();
+				}
+			}
+		}
+
+		/* remove signature of asana reply */
+		var $from = document.querySelectorAll('h3.iw span.gD');
+		if($from.length > 0) {
+			$from = $from[0].getAttribute('email');
+			if( /reply-.+@asana.com/.test($from) ) {
+				var $dots = document.querySelectorAll('.aH1');
+				if( $dots.length > 0 ) {
+					$dots[0].parentNode.click();
+					document.getElementsByClassName('editable')[0].innerHTML = '';
 				}
 			}
 		}
