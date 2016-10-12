7050cd53ad1e6384c9ae96fcdfc7cf34274b30d5
fredpiuma <fredpiuma@gmail.com>
2016-10-12 15:31:28 +0000

    melhorias

--- mailtrack-hacks.user.js
+++ mailtrack-hacks.user.js
@@ -39,19 +39,6 @@
 				}
 			}
 		}
-
-		/* remove signature of asana reply */
-		var $from = document.querySelectorAll('h3.iw span.gD');
-		if($from.length > 0) {
-			$from = $from[0].getAttribute('email');
-			if( /reply-.+@asana.com/.test($from) ) {
-				var $dots = document.querySelectorAll('.aH1');
-				if( $dots.length > 0 ) {
-					$dots[0].parentNode.click();
-					document.getElementsByClassName('editable')[0].innerHTML = '';
-				}
-			}
-		}
 		
 	},500);
 })();
